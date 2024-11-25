7573dd7d4c9863ada9e8fe88cc1d604768ee52ca
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2024-11-25 21:48:23 +0530

    first commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <div className="nav-section">
+      <div className="logo">
+        <h1>Logo</h1>
+      </div>
+      <div className="nav-list">
+        <ul >
+          <li className="active">Home</li>
+          <li>About</li>
+          <li>Classes</li>
+          <li>Teachers</li>
+          <li>Gallery</li>
+          <li>Pages</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="join">
+        <button className="btn">Join Class</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
